a82e8027cd574f66897806954659dd958dbe5779
GereBaraBara <agustin3111926@gmail.com>
2023-08-14 18:08:12 -0300

    14/08/23.3

---
+++ public/app/myapp.js
@@ -0,0 +1,5 @@
+const btn1 = document.getElementById('desplazado1');
+const btn2 = document.getElementById('desplazado2');
+const btn3 = document.getElementById('desplazado3');
+const btn4 = document.getElementById('desplazado4');
+
